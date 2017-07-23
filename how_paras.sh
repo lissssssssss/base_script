d3a0a6d48219a31127aa6227516517160f23fc57
root <root@localhost.localdomain>
2017-07-23 21:44:56 +0800

    how_paras.sh

---
+++ how_paras.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Program:
+#	Program shows the script name,parameters...
+#History:
+#2017/03/17 23:50	Liss	First release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+
+echo "This script name is	==>${0}"
---
+++ readme.md
@@ -0,0 +1,5 @@
+*学习linux bash script的一些简单语法*<br/>
+**2017/03/17 23:54**
+1. how_paras.sh<br/>
+bash script的参数传入<br/>
+
